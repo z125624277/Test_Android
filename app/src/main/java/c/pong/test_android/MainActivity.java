93e07b4bf336de43d2a46fc080060912710aa3ed
PPig <z125624277@gmail.com>
2018-08-02 16:42:05 +0800

    註解物件2018/8/2

--- app/src/main/java/c/pong/test_android/MainActivity.java
+++ app/src/main/java/c/pong/test_android/MainActivity.java
@@ -1,7 +1,8 @@
 package c.pong.test_android;//之前設定的套件名稱(JAVA標準語法)
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 //extends 繼承
@@ -10,6 +11,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         //super就是指定要呼叫父類別的onCreat(),這裡會執行之前的狀態存入此頁面savedInstanceState
         setContentView(R.layout.activity_main);//載入佈局檔:把activity_main.xml畫面顯示出來
+    TextView mytext=(TextView)findViewById(R.id.textView);//取得TextView物件
+    //findViewById會傳回View類別因此需要強制轉換變成(TextView)
+    mytext.setText("我設定的文字");
 
     }
 
